c279ae60a0393270097ddb9b0152c210776baa7c
thegioicuabo <103579376+thegioicuabo@users.noreply.github.com>
2022-05-01 17:12:48 +0700

    Delete Nam directory

--- Nam/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con = new mysqli('localhost','root','','user');
-    if(!$con){
-        die(mysqli_error($con));
-    }
-?>
--- Nam/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['auth_user_id']);
-unset($_SESSION['authuser_name']);
-$_SESSION['status'] = "Logged out Successfully";
-header("Location: Home.php"); 
-exit();
-?>
